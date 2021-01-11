016113c9a0748fa7cf96a0bd7a0b7c84ed4d03cd
nagoyan777 <nagoyan777@hotmail.co.jp>
2021-01-11 23:32:45 +0900

    first example for pulp

---
+++ 2_LP/code21_pulp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from pulp import LpProblem, LpMaximize, LpVariable, LpStatus, value
+#  from pulp import *
+prob = LpProblem(name='LP-Sample', sense=LpMaximize)
+x1 = LpVariable('x1', lowBound=0.0)
+x2 = LpVariable('x2', lowBound=0.0)
+prob += 2*x1 + 3 * x2  # objective function
+prob += x1 + 3*x2 <= 9, 'ineq1'
+prob += x1 + x2 <= 4, 'ineq2'
+prob += x1 + x2 <= 6, 'ineq3'
+print(prob)
+prob.solve() 
+
+print(LpStatus[prob.status])
+print('Optimal value =', value(prob.objective))
+for v in prob.variables():
+    print(v.name, '=', value(v))
+
